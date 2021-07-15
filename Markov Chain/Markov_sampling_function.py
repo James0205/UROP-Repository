ce9c82901adad4d3e047cb217c330ae3e51d1cda
esoreq <35186958+esoreq@users.noreply.github.com>
2021-07-15 20:43:18 +0300

    Update Markov_sampling_function.py
    Try to use the modern generator in numpy also try to use the type hints 
    See my code that does the same as yours but in a slightly different way

--- Markov Chain/Markov_sampling_function.py
+++ Markov Chain/Markov_sampling_function.py
@@ -2,6 +2,47 @@ import string
 import numpy as np
 import random
 
+
+def alternative_sample(states_num: int = 4,
+                       transition_prob: float = None,
+                       noise_scale: float = 0.01,
+                       seed: int = 2021):
+    """[summary]
+
+    Parameters
+    ----------
+    states_num : int, optional
+        [description], by default 4
+    transition_prob : float, optional
+        [description], by default None
+    noise_scale : float, optional
+        [description], by default 0.01
+    seed : int, optional
+        [description], by default 2021
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    rng = np.random.default_rng(seed)
+    if type(transition_prob) == type(None):
+        transition_prob = softmax(rng.normal(size=(4, 4)))
+    noise = rng.normal(0, noise_scale, (states_num, states_num))
+    sample_prob = transition_prob + noise
+    negative_noise = sample_prob.min(axis=0) < 0
+    sample_prob = sample_prob + negative_noise * \
+        np.sign(sample_prob.min(axis=0))*sample_prob.min(axis=0)
+    sample_prob = sample_prob/sample_prob.sum(axis=0)
+    return sample_prob
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+
 #define initial transition probability matrix
 p = np.array([[0.7,0.7,0.7,0.7],[0.1,0.1,0.1,0.1],[0.1,0.1,0.1,0.1],[0.1,0.1,0.1,0.1]])
 
