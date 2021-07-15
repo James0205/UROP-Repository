c86c0bc265187f3ce6cb3e4ebd3d55b2e2e15319
James <77835512+James0205@users.noreply.github.com>
2021-07-15 15:20:24 +0100

    Update Markov_sampling_function.py

--- Markov Chain/Markov_sampling_function.py
+++ Markov Chain/Markov_sampling_function.py
@@ -4,7 +4,7 @@ import random
 
 #define initial transition probability matrix
 p = np.array([[0.1,0.4,0.3,0.2],[0.4,0.2,0.2,0.2],[0.3,0.2,0.2,0.3],[0.2,0.2,0.3,0.3]])
-rows, columns = c.shape
+rows, columns = p.shape
 
 #define noise distribution matrix
 noise_ini = np.random.normal(0,0.01,(rows-1,columns))
@@ -12,16 +12,16 @@ ncol_sum = noise_ini.sum(axis=0)*(-1)
 
 noise = np.append(noise_ini,[ncol_sum],axis= 0)
 
-#columns are froms, rows are tos
+#columns are froms (current state), rows are tos (next state)
 
-def markov(n,m,p,noise): #num of states, num of observations, probability of transition, noise
-    state_space = list(string.ascii_uppercase[:n]) #creating a list of random state_space
-    current_state = random.randrange(n) #randomly choosing the current state
+def markov(n,m,p,noise):                            #num of states, num of observations, probability of transition, noise
+    state_space = list(string.ascii_uppercase[:n])  #creating a list of random state_space
+    current_state = random.randrange(n)             #randomly choosing the current state
     
     z = np.zeros((n,n))
     
-    p_fin = np.clip(p+noise,0,2) #clipping negative values to 0
-    p_fin /= p_fin.sum(axis=0,keepdims=1) #this should give p of col sum 1
+    p_fin = np.clip(p+noise,0,2)            #clipping negative values to 0
+    p_fin /= p_fin.sum(axis=0,keepdims=1)   #this should give p of col sum 1
     
     for i in range(m):
         transition = np.random.choice(list(range(n)), replace = True, p = p_fin[:,n-1])
