bca3054bbac3bb3b90b04487e95a3b5a4d56eb48
James <77835512+James0205@users.noreply.github.com>
2021-07-15 15:48:15 +0100

    Update Markov_simulator_function.py

--- Markov Chain/Markov_simulator_function.py
+++ Markov Chain/Markov_simulator_function.py
@@ -1,12 +1,12 @@
 def simulator(n,num): #number of states, number of samples
     x = np.zeros((n,n))
-    for i in range(f):
-        k = markov(n,1000,p,noise)
-        x += k
+    for i in range(num):
+        piece = sample(n,1000,p,noise)
+        x += piece      #adding samples together
 
     zcol_sum1 = x.sum(axis=0)
-    x/=x.sum(axis=0)
-    
+    x/=x.sum(axis=0)    #generating probability matrix
+       
     return x
  
 #  print(simulator(4,20))
