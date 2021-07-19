a81702f3ef3d0e831df9a41478b716d9f6dd1b60
James <77835512+James0205@users.noreply.github.com>
2021-07-19 15:11:21 +0100

    Update Jupyter_D3.js
    Added transition text indicator on the side

--- D3/Jupyter_D3.js
+++ D3/Jupyter_D3.js
@@ -1,41 +1,59 @@
 //codes to run D3 animation in Jupyter
 
 %%javascript
-//setting up d3
 require.config({
     paths: { 
         d3: 'https://d3js.org/d3.v5.min'
     }
 });
-//getting data from file as json
 $.getJSON("data.json", function(json) {
     var transitionMatrix = json
     var num = transitionMatrix.length
     const alpha = Array.from(Array(num)).map((e, i) => i + 65);
-    const data = alpha.map((x) => String.fromCharCode(x)); //list of states
+    const data = alpha.map((x) => String.fromCharCode(x));
     var currentState = 0;
     var pi = Math.PI //determine Pi
     var m = data.length; // number of states
 
     (function(element) {
-        require(['d3'], function(d3) {              //set up SVG element
+        require(['d3'], function(d3) {
             var svg = d3.select(element.get(0))
                         .append('svg')
                         .attr('width', 750)
                         .attr('height', 750);
 
-            function circleTransition(){            //looping visualisation
-                var filter = svg.append("defs")     //blur filter for glow effect
+            function circleTransition(){
+                var filter = svg.append("defs")
                     .append("filter")
                     .attr("id", "blur")
                     .append("feGaussianBlur")
                     .attr("stdDeviation", 2); 
-                var timeCircle = svg.append("circle")       //transition circle
+                var timeCircle = svg.append("circle")
                     .attr("r", 40);
-                var timeGlowCurrent = svg.append("circle")  //glow ring for current state
+                
+                var timeGlowCurrent = svg.append("circle")
                     .attr("filter", "url(#blur)");   
-                var timeGlowNext = svg.append("circle")     //glow ring for next state
-                    .attr("filter", "url(#blur)"); 
+                
+                var timeGlowNext = svg.append("circle")
+                    .attr("filter", "url(#blur)");
+                
+                var movingCurrentState = svg.append("text")
+                    .attr("font-size",0)
+                    .attr("font-weight",500)
+                    .attr('text-anchor','middle')
+                
+                var movingNextState = svg.append("text")
+                    .attr("font-size",0)
+                    .attr("font-weight",500)
+                    .attr('text-anchor','middle')
+                var text = svg.append("text")
+                    .attr('x',650)
+                    .attr('y',100)
+                    .attr('font-size', 20)
+                    .attr("font-weight",500)
+                    .attr('text-anchor','middle')
+                    .text("TO")
+                
                 var g = svg.selectAll('.someClass')
                     .data(data)
                     .enter()
@@ -51,7 +69,7 @@ $.getJSON("data.json", function(json) {
                       .style("fill", function(d,i){
                 var num = (i+1)*255/m
                 var num2 = 255 - (i+1)*255/m
-                    return "rgb("+num+",200,"+num2+")"});   //for colours
+                    return "rgb("+num+",200,"+num2+")"});
         
                     g.append("text")
                       .style("fill", "white")
@@ -77,34 +95,34 @@ $.getJSON("data.json", function(json) {
                         break;
                         }
                     }
-                    var cx_current =340+200*Math.cos(currentState*2*pi/m)   //define positions of current and next states
+                    var cx_current =340+200*Math.cos(currentState*2*pi/m)
                     var cy_current =260+200*Math.sin(currentState*2*pi/m)
                     var cx_next =340+200*Math.cos(nextState*2*pi/m)
                     var cy_next = 260+200*Math.sin(nextState*2*pi/m)
                   timeCircle
                     .style("opacity",0.5)
                     .attr("fill", "yellow")
-                    .attr('cx', cx_current)      // position the circle at current state
+                    .attr('cx', cx_current)      // position the circle at 40 on the x axis
                     .attr('cy',cy_current)
                     .transition()        // apply a transition
-                    .ease(d3.easePoly)  // apply transition ease
-                    .delay(200)         // apply delay for 200 ms
-                    .duration(800)      // apply it over 800 ms
-                    .attr('r',15)       // radius of circle
+                    .ease(d3.easePoly)
+                    .delay(200)
+                    .duration(800)      // apply it over 2000 milliseconds
+                    .attr('r',15)
                     .attr("fill", "orange")
                     .style("opacity",1.0)
-                    .attr('cx', 340)     // move the circle to 340 on the x axis
-                    .attr('cy',260)     // position the circle at 260 on the y axis
+                    .attr('cx', 340)     // move the circle to 920 on the x axis
+                    .attr('cy',260)     // position the circle at 250 on the y axis
                     .transition()        // apply a transition
-                    .duration(1000)      // apply it over 1000 milliseconds
-                    .attr("fill", "yellow")     //changed colour
-                    .attr('r',40)               //changed radius
+                    .duration(1000)      // apply it over 2000 milliseconds
+                    .attr("fill", "yellow")
+                    .attr('r',40)
                     .style("opacity",0.5)
-                    .attr('cx', cx_next)      // position the circle to next state
+                    .attr('cx', cx_next)      // position the circle at 40 on the x axis
                     .attr('cy', cy_next)
-                    .on("end", repeat);  // when the transition finishes, repeat
+                    .on("end", repeat);  // when the transition finishes start again
               
-                  timeGlowCurrent   //glow indicator of current state
+                  timeGlowCurrent
                     .style("opacity",0)
                     .attr("fill","white")
                     .attr('cx', cx_current)
@@ -123,7 +141,7 @@ $.getJSON("data.json", function(json) {
                     .style("opacity",0)
                     .attr('r',60)
               
-                  timeGlowNext      //glow indicator of next state
+                  timeGlowNext
                     .style("opacity",0)
                     .attr("fill","white")
                     .attr('cx', cx_next)
@@ -141,10 +159,52 @@ $.getJSON("data.json", function(json) {
                     .attr("fill", "white")
                     .style("opacity",0)
                     .attr('r',60)
-              
-                  currentState = nextState; //set next state as current state
+                    
+                  movingCurrentState
+                    .attr('x',600)
+                    .attr('y',100)
+                    .attr('fill', function(d){
+                        var num = (currentState+1)*255/m
+                        var num2 = 255 - (currentState+1)*255/m
+                        return "rgb("+num+",200,"+num2+")"})
+                    .text(data[currentState])
+                    .transition()
+                    .duration(800)
+                    .attr('font-size',40)
+                    .transition()
+                    .duration(800)
+                    .delay(400)
+                    .attr('font-size',0)
+                    
+                  movingNextState
+                    .attr('x',700)
+                    .attr('y',100)
+                    .attr('fill', function(d){
+                        var num = (nextState+1)*255/m
+                        var num2 = 255 - (nextState+1)*255/m
+                        return "rgb("+num+",200,"+num2+")"})
+                    .text(data[nextState])
+                    .transition()
+                    .duration(800)
+                    .attr('font-size',40)
+                    .transition()
+                    .delay(400)
+                    .duration(800)
+                    .attr('font-size',0)
+                
+                  text
+                    .transition()
+                    .duration(800)
+                    .attr('fill','green')
+                    .attr('font-size',30)
+                    .transition()
+                    .duration(800)
+                    .delay(400)
+                    .attr('font-size',20)
+                    .attr('fill','palegreen')
+                  currentState = nextState;
                 };
             };
-        circleTransition()}) //call function
+        circleTransition()})
     })(element);
 });
