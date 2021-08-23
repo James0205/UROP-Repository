c2048f0c16f2150dfd7e7df2386e993d8211ea4a
James <77835512+James0205@users.noreply.github.com>
2021-08-23 14:25:54 +0100

    Update d3fdgraph.py

--- MVVM/d3fdgraph.py
+++ MVVM/d3fdgraph.py
@@ -1,13 +1,52 @@
-import IPython.core.display
+from IPython.display import display, Javascript, HTML, clear_output
+from ipywidgets import interact, fixed
+from datetime import datetime
 from pathlib import Path
-import uuid
+import ipywidgets as widgets
+import IPython.core.display
+import pandas as pd
 import numpy as np
 import string
 import json
+import uuid
 
+def plot_force_directed_graph(transitionMatrix: dict=None, state_name=None, image=None, colour=None,
+                              start_date=datetime(2021, 8, 20),end_date=datetime(2021, 8, 21),interval='120min'):
+    
+    # creating a list of dates and times for slider options
+    dates = pd.date_range(start_date, end_date, freq=interval)[:-1]
+    options = [(date.strftime(' %d %b %Y, %H:%M:%S '),index) for index, date in enumerate(dates)]
+    
+    # make variables global and accessible by dataSelect function
+    global matrixData,stateNameData,imageData,colourData,selection_slider
+    
+    # setting variables values
+    matrixData = transitionMatrix
+    stateNameData = state_name
+    imageData = image
+    colourData = colour
+    selection_slider = widgets.SelectionSlider(
+        options = options,
+        description = 'Dates',
+        orientation = 'horizontal',
+        layout = {'width': '600px'}
+    )
+    
+    # call function whenever slider moves
+    selection_slider.observe(dataSelect,names ='value')
+    
+    # show slider
+    display(selection_slider)
 
-def plot_force_directed_graph(transitionMatrix: float = None, state_name=None, images=None, **kwargs):
-
+def dataSelect(change):
+    # clear output cell
+    clear_output(wait=True)
+    
+    # show slider and graph
+    display(selection_slider)
+    force_directed_graph(matrixData[change.new],stateNameData,imageData,colourData)
+    
+def force_directed_graph(transitionMatrix: float = None, state_name=None, images=None, colour=None, **kwargs):
     # generate random identifier for SVG element, to avoid name clashes if used multiple times in a notebook
     uid = str(uuid.uuid4())
 
@@ -18,17 +57,27 @@ def plot_force_directed_graph(transitionMatrix: float = None, state_name=None, i
     json_nodes = json.dumps(nodesCalibration(transitionMatrix,state_name,images))
     json_links = json.dumps(linksCalibration(transitionMatrix))
     
+    # if colour is given, change variable to true
+    colourGiven = False
+    colourArray = colour
+    if type(colour) != type(None):
+        colourGiven = True
+    colourGiven = json.dumps(colourGiven)
+    colourArray = json.dumps(colourArray)
+    
     # Use different adjustable configuration values
-    config =    {'width': 800,
-                'height': 400,
-                'noderadius': 10,
-                'linkcharge': 600,
-                'linkdistance': 180,
-                'collisionscale': 4,
-                'linkwidthscale': 4,
-                'ticks': 200,
-                'nodes': json_nodes,
-                'links': json_links}
+    config =    {'width': 1000,
+                 'height': 600,
+                 'noderadius': 10,
+                 'linkcharge': -100,
+                 'linkdistance': 180,
+                 'collisionscale': 4,
+                 'linkwidthscale': 4,
+                 'ticks': 200,
+                 'nodes': json_nodes,
+                 'links': json_links,
+                 'colourGiven':colourGiven,
+                 'colourArray':colourArray}
 
     config.update(kwargs)
     js_code = create_d3_fdgraph(uid, config)
@@ -40,7 +89,6 @@ def plot_force_directed_graph(transitionMatrix: float = None, state_name=None, i
     IPython.core.display.display_javascript(IPython.core.display.Javascript(data=js_code))
     pass
 
-
 def create_d3_fdgraph(uid, config):
     js_code = Path('d3fdgraph.js').read_text()
     js_code = js_code.replace('%%unique-id%%', uid)
@@ -49,57 +97,27 @@ def create_d3_fdgraph(uid, config):
 
     return js_code
 
-
-def nodesCalibration(transitionMatrix: float=None, state_name=None, images=None):
-    if type(transitionMatrix) == type(None):
-        transitionMatrix = sample()
+def nodesCalibration(transitionMatrix, state_name, images):
     length = len(transitionMatrix[0])
-    nodes = []
+    
+    # if state_name is not given, returns a list of alphabets
     if type(state_name) == type(None):
-        if type(images) == type(None):
-            for i in range(length):
-                nodes.append({"id":string.ascii_uppercase[i],"index":i,"image": None})
-        else:
-            for i in range(length):
-                nodes.append({"id":string.ascii_uppercase[i],"index":i,"image":images[i]})
-    else:
-        if type(images) == type(None):
-            for i in range(length):
-                nodes.append({"id":state_name[i],"index":i,"image": None})
-        else:
-            for i in range(length):
-                nodes.append({"id":state_name[i],"index":i,"image":images[i]})
+        state_name = string.ascii_uppercase[:length]
+        
+    # if image is not given, returns a list of Nones
+    if  type(images) == type(None):
+        images = [None]*length
+        
+    nodes = []    
+    for i in range(length):
+        nodes.append({"id":state_name[i],"index":i,"image":images[i]})
     return nodes
 
-def linksCalibration(transitionMatrix: float=None, state_name=None, images=None):
-    if type(transitionMatrix) == type(None):
-        transitionMatrix = sample()
+def linksCalibration(transitionMatrix):
     length = len(transitionMatrix[0])
-    transitionMatrix = np.matrix(transitionMatrix)
-    data = np.ndarray.tolist(np.matrix.transpose(transitionMatrix))
     links = []
-    for i in range(length):
-        for j in range(length):
-            if data[i][j] != 0:
-                links.append({"source": i,"target": j,"type": i,"weight": data[i][j]})
+    for j in range(length):
+        for i in range(length):
+            if transitionMatrix[i][j] != 0:
+                links.append({"source": i,"target": j,"type": i,"weight": transitionMatrix[i][j]})
     return links
-                
-                
-def sample(states_num: int = 4,
-                       transition_prob: float = None,
-                       noise_scale: float = 0.01,
-                       seed: int = 2021):
-
-    rng = np.random.default_rng(seed)
-    if type(transition_prob)==type(None):
-        transition_prob = softmax(rng.normal(size=(states_num,states_num)))
-    noise = rng.normal(0,noise_scale,(states_num,states_num))
-    sample_prob = transition_prob + noise
-    negative_noise = sample_prob.min(axis=0)
-    sample_prob = sample_prob + (negative_noise<0)*np.sign(negative_noise)*negative_noise
-    sample_prob = sample_prob/sample_prob.sum(axis=0)
-    return sample_prob
-
-def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
-    return np.exp(x) / np.sum(np.exp(x), axis=0)
